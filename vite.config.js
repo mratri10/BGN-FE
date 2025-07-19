d4060ab1376577087d08950c057f997e3fcaaa97
mratri10 <atriariskaalfa10@gmail.com>
2025-07-19 09:13:01 +0700

    done

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   base: '/BGN-FE/', // sesuai dengan nama repo kamu!
   plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://167.99.76.27:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
