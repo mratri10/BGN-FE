ab108cbddb116f012fdbd042b26b939b486a393b
mratri10 <atriariskaalfa10@gmail.com>
2025-07-19 08:49:52 +0700

    Fix vite base and enable GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path' // ⬅️ pastikan ini di-import
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/BGN-FE/',
+  base: '/BGN-FE/', // sesuai dengan nama repo kamu!
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'), // ⬅️ ini yang penting
-    },
-  },
 })
